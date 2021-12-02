c265aca9ef3e949860a83a7e3c1931c8bd39454a
caquijano <carlosquijano98@gmail.com>
2021-12-02 17:06:32 -0500

    nombre de usuario

--- src/components/Parts/Navbar.tsx
+++ src/components/Parts/Navbar.tsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Logo from "../../images/logo.svg";
 
 function Navbar() {
+  const data:any = JSON.parse(window.localStorage.getItem("SpotifyUser") || " ")
+  console.log(data)
     return (
         <nav className="navbar navbar-light bg-light">
   <div className="container">
     <a className="navbar-brand" href="/">
       <img src={Logo} alt="" width="97"/>
       
+    </a>
+    <a style={{color:"#eb6608"}} className="color-primary navbar-brand" href="/">
+      <img className="mx-3 rounded-circle"  src={data?.images[0]?.url} alt="" width="35"/>
+      {data?.display_name}
     </a>
   </div>
 </nav>
