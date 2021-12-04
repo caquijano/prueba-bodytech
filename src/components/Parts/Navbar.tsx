0f7c13d3c01d8795cc0f82d7809807d13b9dd7ab
caquijano <carlosquijano98@gmail.com>
2021-12-03 20:42:05 -0500

    error inicio

--- src/components/Parts/Navbar.tsx
+++ src/components/Parts/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Logo from "../../images/logo.svg";
 
 function Navbar() {
-  const data:any = JSON.parse(window.localStorage.getItem("SpotifyUser") || " ")
+  const data:any = JSON.parse(window.localStorage.getItem("SpotifyUser") || '{"display_name": "undefined"}')
   console.log(data)
     return (
         <nav className="navbar navbar-light bg-light">
@@ -11,10 +11,17 @@ function Navbar() {
       <img src={Logo} alt="" width="97"/>
       
     </a>
-    <a style={{color:"#eb6608"}} className="color-primary navbar-brand">
+    
+    {
+
+      (data.display_name == "undefined") ?
+      <></>
+      :
+      <a style={{color:"#eb6608"}} className="color-primary navbar-brand">
       <img className="mx-3 rounded-circle"  src={data?.images[0]?.url} alt="" width="35"/>
       {data?.display_name}
     </a>
+    }
   </div>
 </nav>
     )
